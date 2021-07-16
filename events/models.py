f3440ae8ece098e312c33d131658d07dd9c14e92
SteveJops <steven07kolesiko@gmail.com>
2021-07-16 17:32:25 +0300

    has been added events/models

--- events/models.py
+++ events/models.py
@@ -1,3 +1,10 @@
+from asyncio import events
+from distutils.command.upload import upload
 from django.db import models
 
-# Create your models here.
+
+class Event(models.Model):
+    """ Event class """
+    event_image = models.ImageField('Image', upload_to='events/')
+    event_text = models.CharField("Description", max_length=350)
+
--- my_test_blog/settings.py
+++ my_test_blog/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from db_data import DATABASES_DATA
 from pathlib import Path
 from secrets import SECRET_KEY_APP
@@ -39,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog',
+    'events',
 ]
 
 MIDDLEWARE = [
@@ -115,6 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
