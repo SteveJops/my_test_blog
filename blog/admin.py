49309d768b05fbcd6a022bab4dddff0beffad058
SteveJops <steven07kolesiko@gmail.com>
2021-07-16 21:25:55 +0300

    has been added post model and admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,5 @@
+from blog.models import BlogPosts
 from django.contrib import admin
 
 # Register your models here.
+admin.site.register(BlogPosts)
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,18 @@
+from distutils.command import upload
+from tabnanny import verbose
+from turtle import title
 from django.db import models
 
-# Create your models here.
+class BlogPosts(models.Model):
+    """Blog class"""
+    blog_title = models.CharField("Blog Title", max_length=50)
+    blog_date = models.DateTimeField("Blog Date", auto_now_add=True)
+    blog_text = models.TextField("Main Text")
+    blog_image = models.ImageField("Blog Image", upload_to = 'blog/')
+
+    class Meta:
+        verbose_name = "Post"
+        verbose_name_plural = "Posts"
+
+    def __str__(self) -> str:
+        return self.blog_title
