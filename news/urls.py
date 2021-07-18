7c41de0972ef89752ee8a32949f0268585f252a5
SteveJops <steven07kolesiko@gmail.com>
2021-07-18 21:00:45 +0300

    has been added search realization and forms

--- news/urls.py
+++ news/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import NewsList, SearchView, SingleDetail
+from .views import NewsList, SingleDetail
 
 urlpatterns = [
     path('', NewsList.as_view(), name='home'),
-    path('search/', SearchView.as_view(), name='search'),
+    # path('search/', SearchView.as_view(), name='search'),
     path('single/<int:pk>/', SingleDetail.as_view(), name='detail')
 ]
--- news/views.py
+++ news/views.py
@@ -2,14 +2,24 @@ from django.shortcuts import get_object_or_404, render
 from django.views.generic.list import ListView
 from django.views.generic import TemplateView, View, DetailView
 from .models import News
+from django.db.models import Q
 
 # Create your views here.
 
 class NewsList(ListView):
     """ List of articles"""
     model = News
-    queryset = News.objects.all()
+    # queryset = News.objects.all()
     context_object_name = 'news'
+
+    def get_queryset(self):
+        search_query = self.request.GET.get('q', '')
+        if search_query:
+            post = News.objects.filter(Q(title__icontains=search_query)| Q(text_min__icontains=search_query))
+        else:
+           post = News.objects.all()
+
+        return post
 
 
 
@@ -27,16 +37,18 @@ class SingleDetail(DetailView):
     slug_field = "id"
     template_name = "single/single_detail.html"
 
-class SearchView(ListView):
-    """Search Articles"""
-    # paginate_by = 1
-    template_name = 'news_list.html'
+# class SearchView(ListView):
+#     """Search Articles"""
+#     paginate_by = 2
+#     template_name = 'news_list.html'
 
-    def get_queryset(self):
-        query = self.request.GET.get("q").capitalize()
-        context = News.objects.filter(title__icontains=query) 
-        print(context)
-        return context
+#     def get_queryset(self):
+# Q(title__icontains=search_query)| Q(text_min__icontains=search_query)
+#         query = self.request.GET.get("q").capitalize()
+#         print(self.request.GET)
+#         context = News.objects.filter(title__icontains=query) 
+#         print(context)
+#         return context
 
 
 
